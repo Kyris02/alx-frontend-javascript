4be27f3613503ece6871b0dbdd4dc9d70f32e7c3
Tii04 <Tii04@github.com>
2023-04-20 12:09:20 +0200

    Iterator

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,6 +1,6 @@
 export default function createEmployeesObject(departmentName, employees) {
-  const Object = {
+  const object = {
     [departmentName]: employees,
- };
-  return object; 
+  };
+  return object;
 }
