ec3bc549fcc909e7d4b112331cf7d55751c875ed
Tii04 <Tii04@github.com>
2023-04-22 15:22:53 +0200

    eslint format error

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,11 @@
-export default function getFullResponseFromAPI (success){
-	var promise = new Promise((resolve, reject) => {
-		if (success){
-			resolve({status: 200, body: 'Success'});
-		}
-		else {
-			reject(new Error('he fake API is not working currently'));
-		}
-	});
+export default function getFullResponseFromAPI(success) {
+  const promise = new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('he fake API is not working currently'));
+    }
+  });
 
-	return promise;
-	}
+  return promise;
+}
