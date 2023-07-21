4d5c8806686bbef93e8118e9ceff1062f8e49d32
Tii04 <tiisetso.qsebata@gmail.com>
2023-07-21 21:37:29 +0200

    Tasks 0-2

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,9 @@
+import Classroom from './0-classroom'
+
+export default function(initializeRooms){
+    const first = new Classroom(19);
+    const second = new Classroom(20);
+    const third = new Classroom(34);
+
+    return [first, second, third];
+}
